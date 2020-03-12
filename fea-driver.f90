98d3f2a9ff6f15b4ac77d41c29d4863cf06be278
Vincent Bensch <vbensch@gmail.com>
2020-03-12 17:22:34 +0000

    Break

--- fea-driver.f90
+++ fea-driver.f90
@@ -5,13 +5,18 @@ IMPLICIT NONE
     INTEGER, PARAMETER :: iknd=SELECTED_INT_KIND(14)
     
     !Initializing input variables
-    REAL(KIND=rknd) :: rho_0, rho_L, E, L !rho_0 and rho_L are the densities at the fixed and free end of the beam respectivly. E is the modulus of elasticity, and L is the length of the beam.
+    REAL(KIND=rknd) :: rho_o, rho_L, E, L !rho_0 and rho_L are the densities at the fixed and free end of the beam respectivly. E is the modulus of elasticity, and L is the length of the beam.
     INTEGER(KIND=iknd) :: num_elem !Intiger to store the number of elements the beam is to be devided up into.
     
     !Initializing process variables
     REAL(KIND=rknd) :: rho_m !The density slope heading from fixed to free
+    REAL(KIND=rknd) :: dx !element x step allong L
+    REAL(KIND=rknd) :: rho_b !The density y offset accounting for the first element not being #0
+    REAL(KIND=rknd) :: tmp !dummy computation variable
     REAL(KIND=rknd), DIMENSION(:, :), ALLOCATABLE :: M, K !Initialize M and K matricies
-    REAL(KIND=rknd), DIMENSION(2, 2), :: element !Pointer to current matrix
+    REAL(KIND=rknd), DIMENSION(2, 2) :: element, K_elem, M_elem !Sub matrix for current computation
+
+    INTEGER(KIND=iknd) :: i,j !integer for loop iteration
     
 END MODULE fea_init
 
@@ -21,7 +26,7 @@ USE fea_init
 IMPLICIT NONE
     !Get parameters from user
     PRINT *,"Enter the density at x=0 (rho_0):"
-    READ *,rho_0
+    READ *,rho_o
     PRINT *,"Enter the density at x=L (rho_L):"
     READ *,rho_L
     PRINT *,"Enter the beam length (L):"
@@ -31,7 +36,9 @@ IMPLICIT NONE
     PRINT *,"Enter the desired number of elements:"
     READ *,num_elem
     
-    rho_m = (rho_L - rho_0)/L !Computing density slope
+    rho_m = (rho_L - rho_o)/(num_elem - 1) !Computing density slope
+    rho_b = rho_L - rho_m !Computing density y offset
+    dx = L/num_elem !Compute dx
     
     ALLOCATE ( M(num_elem,num_elem) ) !Alocate size for M and K matricies
     ALLOCATE ( K(num_elem,num_elem) )
@@ -39,34 +46,29 @@ IMPLICIT NONE
     M=0 !Set matricies to all 0s
     K=0
     
+    K_elem = 1/6 !Set K element contributions
+    K_elem(1,1) = 1/3 
+    K_elem(2,1) = 1/3
     
+    M_elem = -1  !Set M element contributions
+    M_elem(1,1) = 1
+    M_elem(2,2) = 1
     
-    print '(*(A))', NEW_LINE('a'), NEW_LINE('a')
-    print "(es10.3)",rho_0
-    print "(es10.3)",rho_L
-    print "(es10.3)",L
-    print "(es10.3)",E
-    print "(i7)",num_elem
+    M(1,1) = dx * rho_o / 3 !Set top left matrix element from first beam element
+    DO i = 2,num_elem !Loop to create M matrix
+        j = i-1
+        tmp = dx * ( ( rho_m * i ) + rho_b)
+        M(j:i , j:i) = M_elem * tmp
+    END DO
+    
+    tmp = E/dx !Set contribution for each element in the K matrix
+    K(1,1) = tmp !Set top left matrix element from first beam element
+    DO i = 2,num_elem !Loop to create K matrix
+        j = i-1
+        M(j:i , j:i) = K_elem * tmp
+    END DO
 
-
-CONTAINS
+    CALL(DSBGV('N','L',num_elem,1,1,))
     
-    SUBROUTINE place_elem(elem_in, elem_num_in)
-        REAL(KIND=rknd), INTENT(IN) :: elem_in(2,2) !The incoming sub-matrix will always be shape (2x2)
-        INTEGER(KIND=iknd), INTENT(IN) :: elem_num_in !Element number associated with sub-matrix
-        INTEGER :: i, j !Integers for loop iteration
-        INTEGER :: pos_i, pos_j !Integers for position tracking in input matrix
-        REAL(KIND=rknd) :: tmp !Real for holding intermediate product
-        
-        DO i = 1, 2
-            DO j = 1,2
-                pos_i = elem_num_in - 2 + i
-                pos_j = elem_num_in - 2 + i
-                
-                tmp = ptr(pos_i,pos_j) + elem_in(i,j)
-                ptr(pos_i,pos_j) = tmp
-            END DO
-        END DO
-    END SUBROUTINE place_elem
     
 END PROGRAM fea_driver
